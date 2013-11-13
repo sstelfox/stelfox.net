0ee0cc3c33c8b33d0774b1ea9b43510b104033b8
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-11-13 11:16:03 -0500

    Replaced the xml sitemap builder with a slightly different version

--- lib/default.rb
+++ lib/default.rb
@@ -1,8 +1,13 @@
 
+base_path = File.dirname(__FILE__)
+$LOAD_PATH.unshift(base_path) unless $LOAD_PATH.include?(base_path)
+
 require 'stringex'
+require 'helpers/xml_sitemap_helper'
 
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Tagging
-include Nanoc3::Helpers::XMLSitemap
+
+include XMLSitemapHelper
 
