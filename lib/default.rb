50cc17a5ffa667f00522a94361ae55f7e3c8b993
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-11-13 12:29:34 -0500

    Added tag pages

--- lib/default.rb
+++ lib/default.rb
@@ -8,6 +8,7 @@ require 'helpers/xml_sitemap_helper'
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::Rendering
 
 include XMLSitemapHelper
 
