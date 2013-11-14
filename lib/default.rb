4023b60a5073dcbf31eb81a38ce5d7f9310009d6
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-11-13 21:05:32 -0500

    More work on the site

--- lib/default.rb
+++ lib/default.rb
@@ -3,9 +3,11 @@ base_path = File.dirname(__FILE__)
 $LOAD_PATH.unshift(base_path) unless $LOAD_PATH.include?(base_path)
 
 require 'stringex'
+require 'titleize'
 require 'helpers/xml_sitemap_helper'
 
 include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Breadcrumbs
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Text
