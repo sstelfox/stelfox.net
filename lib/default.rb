1743f29a007f24ad5d65a362b3e6550b6a9489a2
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-12-07 09:21:36 -0500

    Lot of refactoring got some sketchiness out of my ruleset

--- lib/default.rb
+++ lib/default.rb
@@ -4,8 +4,9 @@ $LOAD_PATH.unshift(base_path) unless $LOAD_PATH.include?(base_path)
 
 require 'stringex'
 require 'titleize'
-require 'helpers/xml_sitemap_helper'
 require 'helpers/post_helpers'
+require 'helpers/short_code_helper'
+require 'helpers/xml_sitemap_helper'
 
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Breadcrumbs
@@ -15,5 +16,6 @@ include Nanoc3::Helpers::Text
 include Nanoc3::Helpers::Rendering
 
 include PostHelpers
+include ShortCodeHelper
 include XMLSitemapHelper
 
