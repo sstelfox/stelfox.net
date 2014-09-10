c86414694a5101319d1aee856d72d2099e3074e4
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-09-10 11:34:08 -0400

    Minor improvement

--- content/api/v1/ip_check.php
+++ content/api/v1/ip_check.php
@@ -3,7 +3,7 @@
   //  available.
   $ips = array(
     "direct_ip" => $_SERVER['REMOTE_ADDR']
-  );  
+  );
 
   // If we detect a forwarded for IP address add them in
   if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -18,6 +18,10 @@
   // Cloudflare headers
   if (!empty($_SERVER['CF-Connecting-IP'])) {
     $ips['cloudflare_connecting_ip'] = $_SERVER['CF-Connecting-IP'];
+  }
+
+  if (!empty($_SERVER['HTTP_CF_IPCOUNTRY'])) {
+    $ips['cloudflare_geolocation'] = $_SERVER["HTTP_CF_IPCOUNTRY"];
   }
 
   echo json_encode($ips);
