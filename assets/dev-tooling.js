1608693ad7012f7fb149b8e8496d2e550f91e9be
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-07-17 14:39:01 -0400

    feat: bring in more tooling for grid diagnostics

--- assets/dev-tooling.js
+++ assets/dev-tooling.js
@@ -19,6 +19,7 @@ function setupContainerIndicator() {
   setContainerVisibility(showContainer);
 }
 
+
 function setParameterVisibility(data) {
   var showParameter = data;
   var e = document.querySelector("#parameter-area");
@@ -40,10 +41,46 @@ function setupParameterIndicator() {
   setParameterVisibility(showParameter);
 }
 
+
+const padOptions = ["0", "1", "2", "3", "4", "5", "6", "8", "10", "12", "16", "20", "24", "32", "40", "48", "56", "64"];
+const colOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+const gapOptions = ["0", "1", "2", "3", "4", "5", "6", "8", "10", "12", "16"];
+
+/* helper function to remove a class attribute from an element */
+function removeClassWith(e, part) {
+  var regx = new RegExp('(^|\\s)\\S*' + part + '\\S*', 'g');
+  e.className = e.className.replace(regx, '');
+}
+
+function setGridVisibility(data) {
+  var showGrid = data;
+  var e = document.querySelector("#grid-area");
+  if (e.classList.contains("hidden") == showGrid) {
+    e.classList.toggle("hidden");
+  }
+
+  sessionStorage.setItem("showGrid", showGrid);
+}
+
+function changeGridVisibility() {
+  var e = document.querySelector("#grid-area");
+  var showGrid = e.classList.contains("hidden") ? true : false;
+  setGridVisibility(showGrid);
+}
+
+/* load variables from local storage */
+function setupGridIndicator() {
+  var showGrid = sessionStorage.getItem("showGrid") === "true";
+  setGridVisibility(showGrid);
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   setupContainerIndicator();
   document.getElementById('container-menu').addEventListener('click', changeContainerVisibility);
 
   setupParameterIndicator();
   document.getElementById('parameter-menu').addEventListener('click', changeParameterVisibility);
+
+  setupGridIndicator();
+  document.getElementById('grid-menu').addEventListener('click', changeGridVisibility);
 });
