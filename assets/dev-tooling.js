7d78073fe2cf7feec3332eaf734dde719defc393
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-07-16 23:12:25 -0400

    feat: get the dev tooling working with CSP semantics

--- assets/dev-tooling.js
+++ assets/dev-tooling.js
@@ -1,23 +1,49 @@
 function setContainerVisibility(data) {
-    var showContainer = data;
-    var e = document.querySelector("#container-area");
-    if (e.classList.contains("hidden") == showContainer) {
-        e.classList.toggle("hidden");
-    }
-    sessionStorage.setItem("showContainer", showContainer);
+  var showContainer = data;
+  var e = document.querySelector("#container-area");
+  if (e.classList.contains("hidden") == showContainer) {
+    e.classList.toggle("hidden");
+  }
+  sessionStorage.setItem("showContainer", showContainer);
 }
 
 function changeContainerVisibility() {
-    var e = document.querySelector("#container-area");
-    var showContainer = e.classList.contains("hidden") ? true : false;
-    setContainerVisibility(showContainer);
+  var e = document.querySelector("#container-area");
+  var showContainer = e.classList.contains("hidden") ? true : false;
+  setContainerVisibility(showContainer);
 }
 
+/* load variables from session storage */
 function setupContainerIndicator() {
-    /* load variables from session storage */
-    var showContainer = sessionStorage.getItem("showContainer") === "true";
-
-    setContainerVisibility(showContainer);
+  var showContainer = sessionStorage.getItem("showContainer") === "true";
+  setContainerVisibility(showContainer);
 }
 
-window.addEventListener("load", setupContainerIndicator);
+function setParameterVisibility(data) {
+  var showParameter = data;
+  var e = document.querySelector("#parameter-area");
+  if (e.classList.contains("hidden") == showParameter) {
+    e.classList.toggle("hidden");
+  }
+  sessionStorage.setItem("showParameter", showParameter);
+}
+
+function changeParameterVisibility() {
+  var e = document.querySelector("#parameter-area");
+  var showParameter = e.classList.contains("hidden") ? true : false;
+  setParameterVisibility(showParameter);
+}
+
+/* load variables from session storage */
+function setupParameterIndicator() {
+  var showParameter = sessionStorage.getItem("showParameter") === "true";
+  setParameterVisibility(showParameter);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  setupContainerIndicator();
+  document.getElementById('container-menu').addEventListener('click', changeContainerVisibility);
+
+  setupParameterIndicator();
+  document.getElementById('parameter-menu').addEventListener('click', changeParameterVisibility);
+});
