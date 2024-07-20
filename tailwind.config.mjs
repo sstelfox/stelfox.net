3063ee515f2554212e3727dfd353150ecada14fe
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-07-20 15:48:21 -0400

    chore: add future work in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 export default {
+  // todo(sstelfox): In a production build I want this to only look at the
+  // generated public directory so it can tree-shake out CSS styles that aren't
+  // actually used anywhere.
   content: ['content/**/*.md', 'layouts/**/*.html'],
   darkMode: 'class',
   theme: {
