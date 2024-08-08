68813f9c5c949c5a94886be489cdf1fafda8dfa3
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-08-07 21:00:21 -0400

    feat: a lot of tweaks to my site

--- themes/stelfox-picocss/assets/js/deferred.js
+++ themes/stelfox-picocss/assets/js/deferred.js
@@ -1,85 +1,10 @@
-/* todo(sstelfox): rewrite the theme selection as the persistence isn't working */
-
-var themeDarkIcon = document.getElementById('theme-toggle-dark');
-var themeLightIcon = document.getElementById('theme-toggle-light');
-var themeSystemIcon = document.getElementById('theme-toggle-system');
-
-function setThemeVisibility(themeParameter) {
-  switch (themeParameter) {
-    case 'system':
-      themeSystemIcon.classList.toggle('hidden');
-      break;
-    case 'light':
-      themeLightIcon.classList.toggle('hidden');
-      break;
-    case 'dark': themeDarkIcon.classList.toggle('hidden');
-  }
-}
-
-function changeTheme() {
-  if (themeSystemIcon.classList.contains('hidden') == false) {
-    themeSystemIcon.classList.toggle('hidden');
-    themeLightIcon.classList.toggle('hidden');
-
-    localStorage.setItem('color-theme', 'light');
-    document.documentElement.classList.remove('dark');
-  } else if (themeLightIcon.classList.contains('hidden') == false) {
-    themeLightIcon.classList.toggle('hidden');
-    themeDarkIcon.classList.toggle('hidden');
-
-    localStorage.setItem('color-theme', 'dark');
-    document.documentElement.classList.add('dark');
-  } else if (themeDarkIcon.classList.contains('hidden') == false) {
-    themeDarkIcon.classList.toggle('hidden');
-    themeSystemIcon.classList.toggle('hidden');
-
-    localStorage.removeItem('color-theme');
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.classList.add('dark');
-    }
-  } else {
-    /* default fallback to system preference */
-    themeSystemIcon.classList.remove('hidden');
-    themeLightIcon.classList.add('hidden');
-    themeDarkIcon.classList.add('hidden');
-
-    localStorage.removeItem('color-theme');
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.classList.add('dark');
-    }
-  }
-}
-
-if (localStorage.getItem('color-theme') === 'dark') {
-  var themeParameter = 'dark';
-} else if (localStorage.getItem('color-theme') === 'light') {
-  var themeParameter = 'light';
-} else {
-  var themeParameter = 'system';
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  setThemeVisibility(themeParameter);
-  document.getElementById('theme-toggle').addEventListener('click', changeTheme);
-});
-
-
 /* If the page has mermaid loaded, detect it and set the theme appropriately. */
 if (typeof mermaid !== 'undefined') {
-  var isDarkTheme = document.documentElement.classList.contains('dark');
-
-  /* todo(sstelfox): the theme toggle needs to update the marmaid theme as well... */
-  if (isDarkTheme) {
-    var chosenTheme = 'dark';
-  } else {
-    var chosenTheme = 'default';
-  }
-
   // Should do a whole lot more in the theming department once I have a color scheme:
   // * https://mermaid.js.org/config/theming.html
   mermaid.initialize({
-    "darkMode": isDarkTheme,
-    "startOnLoad": true,
-    "theme": chosenTheme,
+    'darkMode': true,
+    'startOnLoad': true,
+    'theme': 'dark',
   });
 }
--- themes/stelfox-picocss/assets/js/dev-tooling.js
+++ themes/stelfox-picocss/assets/js/dev-tooling.js
@@ -1,86 +1,33 @@
-function setContainerVisibility(data) {
-  var showContainer = data;
-  var e = document.querySelector("#container-area");
-  if (e.classList.contains("hidden") == showContainer) {
-    e.classList.toggle("hidden");
-  }
-  sessionStorage.setItem("showContainer", showContainer);
+function currentElVisibility(id) {
+  return document
+    .getElementById(id)
+    .classList
+    .contains('hidden') ? true : false;
 }
 
-function changeContainerVisibility() {
-  var e = document.querySelector("#container-area");
-  var showContainer = e.classList.contains("hidden") ? true : false;
-  setContainerVisibility(showContainer);
+function initElVisibility(id) {
+  var savedCfgVis = sessionStorage.getItem('display-' + id) === 'true';
+  setElVisibility(id, savedCfgVis);
 }
 
-/* load variables from session storage */
-function setupContainerIndicator() {
-  var showContainer = sessionStorage.getItem("showContainer") === "true";
-  setContainerVisibility(showContainer);
-}
+function setElVisibility(id, tgtVisibility) {
+  var tgtEl = document.getElementById(id);
 
-
-function setParameterVisibility(data) {
-  var showParameter = data;
-  var e = document.querySelector("#parameter-area");
-  if (e.classList.contains("hidden") == showParameter) {
-    e.classList.toggle("hidden");
-  }
-  sessionStorage.setItem("showParameter", showParameter);
-}
-
-function changeParameterVisibility() {
-  var e = document.querySelector("#parameter-area");
-  var showParameter = e.classList.contains("hidden") ? true : false;
-  setParameterVisibility(showParameter);
-}
-
-/* load variables from session storage */
-function setupParameterIndicator() {
-  var showParameter = sessionStorage.getItem("showParameter") === "true";
-  setParameterVisibility(showParameter);
-}
-
-
-const padOptions = ["0", "1", "2", "3", "4", "5", "6", "8", "10", "12", "16", "20", "24", "32", "40", "48", "56", "64"];
-const colOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
-const gapOptions = ["0", "1", "2", "3", "4", "5", "6", "8", "10", "12", "16"];
-
-/* helper function to remove a class attribute from an element */
-function removeClassWith(e, part) {
-  var regx = new RegExp('(^|\\s)\\S*' + part + '\\S*', 'g');
-  e.className = e.className.replace(regx, '');
-}
-
-function setGridVisibility(data) {
-  var showGrid = data;
-  var e = document.querySelector("#grid-area");
-  if (e.classList.contains("hidden") == showGrid) {
-    e.classList.toggle("hidden");
+  if (currentElVisibility(id) !== tgtVisibility) {
+    tgtEl.classList.toggle('hidden');
   }
 
-  sessionStorage.setItem("showGrid", showGrid);
+  sessionStorage.setItem('display-' + id, tgtVisibility);
 }
 
-function changeGridVisibility() {
-  var e = document.querySelector("#grid-area");
-  var showGrid = e.classList.contains("hidden") ? true : false;
-  setGridVisibility(showGrid);
-}
-
-/* load variables from local storage */
-function setupGridIndicator() {
-  var showGrid = sessionStorage.getItem("showGrid") === "true";
-  setGridVisibility(showGrid);
+function toggleElVisibility(id) {
+  setElVisibility(id, !currentElVisibility(id));
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  setupContainerIndicator();
-  document.getElementById('container-menu').addEventListener('click', changeContainerVisibility);
+  initElVisibility('page-details');
 
-  setupParameterIndicator();
-  document.getElementById('parameter-menu').addEventListener('click', changeParameterVisibility);
-
-  setupGridIndicator();
-  document.getElementById('grid-menu').addEventListener('click', changeGridVisibility);
+  document
+    .getElementById('page-details-toggle-btn')
+    .addEventListener('click', () => toggleElVisibility('page-details'));
 });
